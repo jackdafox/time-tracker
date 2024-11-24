3aeab792dce1aa6715b7914361d7b87eb51813c3
jackdafox <83963164+jackdafox@users.noreply.github.com>
2024-11-24 20:45:39 +0800

    Replace useRouter with redirect for homepage navigation after successful login

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -2,14 +2,13 @@
 
 import { signIn } from "next-auth/react";
 import { useState } from "react";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
-  const router = useRouter();
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -27,7 +26,7 @@ export default function LoginPage() {
     if (result?.error) {
       setError("Wrong Credentials");
     } else {
-      router.push("/"); // Redirect to the homepage after successful login
+      redirect("/");
     }
   };
 
