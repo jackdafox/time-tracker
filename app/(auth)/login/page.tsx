29d8154be91e98f3b0347afb3061bd82abfdb13e
jackdafox <83963164+jackdafox@users.noreply.github.com>
2024-11-25 19:15:00 +0800

    Refactor login page component: rename function to 'page' and import LoginPage component

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,8 +1,9 @@
+import LoginPage from "@/components/page/LoginPage";
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 
-export default async function LoginPage() {
+export default async function page() {
   const session = await getServerSession(authOptions);
 
   if (session) {
